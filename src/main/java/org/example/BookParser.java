c7777e2a96f96f5536a2c0b3eddc39440765f37f
dante <shaitanaleksander@gmail.com>
2023-10-11 00:06:49 +0300

    have fun :>

--- src/main/java/org/example/BookParser.java
+++ src/main/java/org/example/BookParser.java
@@ -9,23 +9,12 @@ import java.util.*;
 public class BookParser {
     private static final String CHAR_TO_REMOVE = "!@#%&*_,./=+";
 
-    public List<String> getText(File file) throws FileNotFoundException {
+    public List<String> getWords(File file) throws FileNotFoundException {
         return new BufferedReader(new FileReader(file))
                 .lines()
                 .flatMap(line -> Arrays.stream(line.split(" ")))
                 .map(word -> word.replaceAll(CHAR_TO_REMOVE, ""))
                 .filter(word -> word.length() > 2)
                 .toList();
-    }
-
-    public List<String> popularWorld(List<String> world, int amount) {
-        return world.stream()
-                .distinct()
-                .sorted(Comparator.comparingInt(s -> Collections.frequency(world, s)).reversed())
-                .limit(amount).toList();
-    }
-
-    public int uniqWords(List<String> words) {
-        return new HashSet<>(words).size();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,27 +7,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-        private static final String path = "src/main/resources/";
-    private static final String fileName = "%s_statistic.txt ";
-    public static void main(String[] args) throws IOException {
+    private static final String dirLocation = "src/main/resources/";
+    private static final String namePrefix = "%s_statistic.txt";
+    public static void main(String[] args)  {
 
         Scanner scanner = new Scanner(System.in);
+        boolean exit  = false;
 
-        String command = scanner.next();
         BookParser parser = new BookParser();
+        StatisticService statisticService = new StatisticService();
 
-        while (!command.equals("exit")) {
+        while (!exit) {
 
-            File book = new File(path + command);
-            List<String> text = parser.getText(book);
-            List<String> top = parser.popularWorld(text, 3);
-            int uniqWords = parser.uniqWords(text);
+            System.out.println(Messages.PLEASE_ENTER_FILE_NAME);
+            File book = new File(dirLocation + scanner.next());
+            List<String> text = null;
+            try {
+                text = parser.getWords(book);
+            } catch (FileNotFoundException e) {
+               System.err.println(Messages.FILE_NOT_FOUND);
+               continue;
+            }
+
+            List<String> top = statisticService.popularWorld(text, 3); // TODO: 11.10.2023  hardcoded var
+            int uniqWords = statisticService.uniqWords(text);
             Statistic statistic = new Statistic(top, uniqWords);
 
-            new StatisticService().save(statistic, new File(String.format(fileName, book.getName())));
+            try {
+                new StatisticService().save(statistic, new File(String.format(dirLocation + namePrefix, book.getName())));
+            } catch (IOException e) {
+                System.err.println(Messages.INTERNAL_ERROR);
+                break;
+            }
             System.out.println(statistic);
-            System.out.println(" type exit ot exit :-)");
-            command = scanner.next();
+            System.out.println(Messages.EXIT);
+            exit = !scanner.next().equals("n"); // TODO: 11.10.2023  hardcoded var
         }
+        scanner.close();
     }
 }
